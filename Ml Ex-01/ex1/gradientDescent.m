1b52bcdb81442bed9be65e1735d4434db7d927f4
Prince Mathew <mail4prince92@gmail.com>
2017-11-02 23:09:29 +0530

    Update gradientDescent.m

--- Ml Ex-01/ex1/gradientDescent.m
+++ Ml Ex-01/ex1/gradientDescent.m
@@ -1,4 +1,4 @@
-ssfunction [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
